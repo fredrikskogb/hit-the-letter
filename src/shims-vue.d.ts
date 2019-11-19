2f43c4b1f27fc70c06d4c9a195b72bea97c48657
Fredrik Skogberg <skogberg.fredrik@gmail.com>
2019-11-19 20:36:09 +0100

    init commit, set up basic boiler plate

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,3 +1,4 @@
+// Telling vue how to handle external code.
 declare module '*.vue' {
   import Vue from 'vue'
   export default Vue
--- src/store/index.ts
+++ src/store/index.ts
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
-
+// Used for sending values to distant components without having to use props and emits
 export default new Vuex.Store({
   state: {
   },
