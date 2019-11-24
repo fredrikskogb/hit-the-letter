ade88c4afe7bc0551ff1b8971d9f6005edc08436
Fredrik Skogberg <skogberg.fredrik@gmail.com>
2019-11-25 00:47:19 +0100

    added more properties to vuex state and getters

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -5,14 +5,21 @@ import { user } from '@/mocks/user.ts';
 // Used for sending values to distant components without having to use props and emits
 
 const state = {
-    users: [],
+    allUsers: [] as IUser[],
     user: {} as IUser,
-    loggedInStatus: false
+    loggedInStatus: false,
+
+    highscore: {},
+    allHighscores: []
 };
 
 const getters = {
     user: (state: { user: IUser; }) => state.user,
-    getLoggedInStatus: (state: { loggedInStatus: boolean; }) => state.loggedInStatus
+    allUsers: (state: { allUsers: IUser[]; }) => state.allUsers,
+    getLoggedInStatus: (state: { loggedInStatus: boolean; }) => state.loggedInStatus,
+
+    highscore: (state: { highscore: {}; }) => state.highscore,
+    allHighscores: (state: { allHighscores: []; }) => state.allHighscores,
 };
 
 const actions = {
