b853b2ef1867f5295bdc46fe237cfccc897f1e5b
Fredrik Skogberg <skogberg.fredrik@gmail.com>
2019-11-26 15:12:01 +0100

    fixed issue with api link

--- src/repository/services/users.ts
+++ src/repository/services/users.ts
@@ -4,7 +4,7 @@ const resource = '/user';
 
 export default {
     loginCustomer(email: string, password: string): any {
-        return Repository.delete(`${resource}/login.php?email=${email}&password=${password}`);
+        return Repository.get(`${resource}/login.php?email=${email}&password=${password}`);
     },
 
     setCustomer(email: string, password: string) {
