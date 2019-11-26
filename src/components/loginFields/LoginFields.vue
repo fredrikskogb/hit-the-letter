b2e71a21ef2edd25cc94b577f1954d44bb4a7a62
Lukas Lennstring <lukas.lennstring@gmail.com>
2019-11-26 15:10:48 +0100

    Try the waters making a new basic login component

---
+++ src/components/loginFields/LoginFields.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="login-input-fields">
+    <input name="username" type="text" />
+    <input name="password" type="password" />
+    <button>Log in</button>
+    <button>Sign up</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+
+    }
+  },
+  
+  props: [],
+
+  components: {
+
+  },
+
+  methods: {
+
+  },
+
+  created () {
+    console.log("login fields created");
+  },
+})
+</script>
+
+<style lang="less" scoped>
+  @import url('../../styles/main.less');
+</style>
