e3239aca1d8e97cad27f5614a69ff355e674d5d9
lunasdude <lukas.lennstring@gmail.com>
2020-05-09 16:50:05 +0200

    actual data

--- src/main.ts
+++ src/main.ts
@@ -7,16 +7,18 @@ import store from './store'
 Vue.config.productionTip = false
 
 // Initialize Firebase
-let config = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SEND_ID"
+const firebaseConfig = {
+  apiKey: "AIzaSyBfQ4sPkYhArzepkzo4uKDHYloPHt4LblM",
+  authDomain: "hit-the-letter.firebaseapp.com",
+  databaseURL: "https://hit-the-letter.firebaseio.com",
+  projectId: "hit-the-letter",
+  storageBucket: "hit-the-letter.appspot.com",
+  messagingSenderId: "991065049074",
+  appId: "1:991065049074:web:0eb457e73b8b21cf748d6b",
+  measurementId: "G-ZM80VG8X2V"
 };
 
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
