36d337fecdd842afbb159c86b5e7100fe3bbabbc
lunasdude-caspeco <lukas.lennstring+caspeco@gmail.com>
2020-05-09 16:37:13 +0200

    Initialize and install firebase

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import Vue from 'vue'
+import firebase from 'firebase'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+
+// Initialize Firebase
+let config = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SEND_ID"
+};
+
+firebase.initializeApp(config);
 
 new Vue({
   router,
